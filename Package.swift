c215fb49a937e4d193e2b81876572fddba261e6a
zegolibrary <developer@zego.im>
2023-03-08 14:10:07 +0800

    🔖 Release 2.7.0

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ZIM",
-            url: "https://artifact-node.zego.cloud/generic/swiftpm/public/zim-ios/ZIM.xcframework.zip?version=2.6.0",
-            checksum: "0ed3d18fa73a8ba5b0bdcbc2a7e371f55c70eb4b129df8ac177f6e6e5ee5264f"
+            url: "https://artifact-node.zego.cloud/generic/swiftpm/public/zim-ios/ZIM.xcframework.zip?version=2.7.0",
+            checksum: "c8da1263f1ee9112d94d4ea922df2f7212d81ecc91b0978776e4c52f12165308"
         )
     ]
 )
